64db9e44f5d73abb2b37e7099e0307b2020275f8
manxiaoman <manxiaoman@pinduoduo.com>
2026-01-14 16:08:56 +0800

    更新构建脚本

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+// 1. 定义一个变量，通过类型断言强制获取 __dirname
+// @ts-ignore - 忽略下一行的类型检查
+const rootDir = typeof __dirname !== 'undefined' ? __dirname : (globalThis as any).__dirname || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -12,7 +17,7 @@ export default defineConfig({
       // 2. new URL(...).pathname 转换为系统绝对路径
       // 3. replace(...) 去掉文件名，只保留目录
       // 4. 拼接上 node_modules 的真实路径
-      'v-code-diff': './node_modules/v-code-diff/dist/index.es.js'
+      'v-code-diff': rootDir  + '/node_modules/v-code-diff/dist/index.es.js'
     }
   },
   define: {
