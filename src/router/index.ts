47d96f14b2c7794f717d2a593dfb49c671bc401b
manxiaoman <manxiaoman@pinduoduo.com>
2026-01-14 14:36:25 +0800

    更新图标和路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,6 @@ import DateToolView from '../views/DateToolView.vue'
 import WorkTimeCalcView from '../views/WorkTimeCalcView.vue'
 import CodeDiffToolView from '../views/CodeDiffToolView.vue'
 import RevertDataView from '../views/RevertDataView.vue'
-import XunFeiAIView from '../views/XunFeiAIView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
@@ -17,7 +16,6 @@ const routes = [
   { path: '/workTimeCalcCode', component: WorkTimeCalcView },
   { path: '/codeDiffTool', component: CodeDiffToolView },
   { path: '/revertData', component: RevertDataView },
-  { path: '/xunFeiAI', component: XunFeiAIView },
 ]
 
 const router = createRouter({
