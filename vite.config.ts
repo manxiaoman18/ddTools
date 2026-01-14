3a6ce4a54346f75bd97e943b419f6ba7b010df9c
manxiaoman <manxiaoman@pinduoduo.com>
2026-01-14 15:28:36 +0800

    更新构建脚本

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'v-code-diff': path.resolve(__dirname, './node_modules/v-code-diff/dist/index.es.js')
+    }
+  },
   base: "/",
   define: {
     // 注入构建时间变量（格式：YYYY-MM-DD HH:mm:ss）
